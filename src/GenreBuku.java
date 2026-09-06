67fb6d5b2d5e388825f551a8c9c16ed65e554761
noviasitizahra <noviasitizahra2006>
2026-09-06 18:23:39 +0700

    genre

---
+++ src/GenreBuku.java
@@ -0,0 +1,11 @@
+public class GenreBuku {
+    private String namaGenre;
+
+    public GenreBuku(String namaGenre){
+        this.namaGenre = namaGenre;
+    }
+
+    public String getNamaGenre(){
+        return namaGenre;
+    }
+}
